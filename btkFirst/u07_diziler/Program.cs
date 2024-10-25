c6781ae894d427d22edd77b1fc1ee7b553fc0b72
Sezai Gürle <sezaigurle35@gmail.com>
2024-10-25 16:05:53 +0300

    08 simp

--- btkFirst/u07_diziler/Program.cs
+++ btkFirst/u07_diziler/Program.cs
@@ -3,3 +3,15 @@
 foreach(int s in sayilar){
 Console.WriteLine($"{s},");
 }
+
+int [] sayilar2 = new int [5];
+
+sayilar2[0] = 3;
+Console.WriteLine("\nDizinin 1. indeksteki elemani kaç olsun?");
+sayilar2[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\n2.dizinin elemanlari:");
+
+foreach(int s in sayilar2){
+Console.WriteLine($"{s},");
+}
---
+++ btkFirst/u08_metinDizisi/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Bir metin girin:");
+string metin = Console.ReadLine();
+Console.WriteLine("\n\n");
+
+int index = 0;
+foreach(char c in metin){
+    char c2 = c;
+    if(index == 0)
+    c2 = char.ToUpper(c);
+    else if(metin[index-1] == ' ')
+    c2 = char.ToUpper(c);
+    Console.Write(c2 + " ");
+    index++;
+}
