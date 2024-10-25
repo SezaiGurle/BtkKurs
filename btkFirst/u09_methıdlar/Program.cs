ced0dce9c5e27ba0153830b772f4b73f841b9aba
Sezai Gürle <sezaigurle35@gmail.com>
2024-10-25 17:02:36 +0300

    Değişken metotlarla çalışıldı

---
+++ btkFirst/u09_methıdlar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sayıları girin (virgülle ayırarak):");
+        string input = Console.ReadLine();
+
+        // Kullanıcının girdiği sayıları diziye dönüştür
+        int[] sayilar = input.Split(',')
+                             .Select(int.Parse)
+                             .ToArray();
+
+        double sonuc = OrtalamaHesapla(sayilar);
+        Console.WriteLine($"Ortalama Sonucu: {sonuc}");
+    }
+
+    static double OrtalamaHesapla(params int[] sayilar)
+    {
+        int toplam = 0;
+        foreach (int s in sayilar)
+        {
+            toplam += s;
+        }
+        return toplam / (double)sayilar.Length;
+    }
+}
