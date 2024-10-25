5f961a1527b2e3a751595176243af9b4941a8abb
Sezai Gürle <sezaigurle35@gmail.com>
2024-10-25 15:32:23 +0300

    have coded some loops simp

--- btkFirst/u05_haftanınGünleri/Program.cs
+++ btkFirst/u05_haftanınGünleri/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Haftanin Kaçinci Günü?");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch(day){
+    case 0: Console.WriteLine("Pazar"); break;
+    case 1: Console.WriteLine("Pazartesi"); break;
+    case 2: Console.WriteLine("Sali"); break;
+    case 3: Console.WriteLine("Carsamba"); break;
+    case 4: Console.WriteLine("Persembe"); break;
+    case 5: Console.WriteLine("Cuma"); break;
+    case 6: Console.WriteLine("Cumartesi"); break;
+    default: Console.WriteLine("Hatali Day"); break;
+}
+
+switch(day)
+{
+    case 0:
+    case 1:
+    case 2:
+    case 3: Console.WriteLine("Haftanin 1. yarisi");break;
+    case 4:
+    case 5:
+    case 6: Console.WriteLine("Haftanin ikinci yarisi");break;
+    default: Console.WriteLine("Hatali Day");break;
+}
---
+++ btkFirst/u06_loops/Program.cs
@@ -0,0 +1,19 @@
+﻿for(int i=0; i<10; i++){
+    Console.WriteLine('*');
+}
+
+int k = 0;
+while(k<10){
+    Console.WriteLine(k + ' ');
+    ++k;
+}
+
+
+﻿string girilenPinKodu="";
+int deneme=1;
+do
+{
+    Console.WriteLine($"PİN Kodunu Girin({deneme}.deneme):");
+    girilenPinKodu = Console.ReadLine();
+    deneme++;
+} while (girilenPinKodu != "1234");
